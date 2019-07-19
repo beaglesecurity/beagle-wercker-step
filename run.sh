a77bf2abf9cacd4dffb6d6458e2fe5999af5a96e
manindar mohan <53038087+manindar-mohan@users.noreply.github.com>
2019-07-19 16:26:06 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 URL=https://beagle-tvm-api.appfabs.com/v1/test/start/
-curl --silent --header "Content-Type: application/json" --request POST --data "{\"user_token\":\"$USER_TOKEN\",\"application_token\":\"$APPLICATION_TOKEN\"}" $URL | \
+curl --silent --header "Content-Type: application/json" --request POST --data "{\"ACCESS_TOKEN\":\"$ACCESS_TOKEN\",\"APPLICATION_TOKEN\":\"$APPLICATION_TOKEN\"}" $URL | \
     sed -e 's/[{}]/''/g' | awk 'BEGIN { FS="\""; RS="," }; { if ($2 == "status") { print "Status : "$4} if ($2 == "message") { print "Message : "$4} }'
